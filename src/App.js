5114ce2ef2de0f039a51518e40aed98cd84ba94a
Yano Omurai <52166982+thanhtaimrjoe@users.noreply.github.com>
2022-06-12 19:43:54 +0700

    Initial project commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='container'>
+      <text>Hello ReactJS</text>
+    </div>
+  );
+}
+
+export default App;
