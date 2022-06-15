d05d8bf1fec8e127dcd0a06f3fb32872e5624ff0
Yano Omurai <52166982+thanhtaimrjoe@users.noreply.github.com>
2022-06-15 19:38:37 +0700

    no message

--- src/App.js
+++ src/App.js
@@ -1,33 +1,49 @@
 import React, { Component } from "react";
 import "./App.css";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, NavLink } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
+import Contact from "./components/Contact";
 
+const MenuLink = ({ label, to }) => {
+  return (
+    <Route
+      path={to}
+      children={({ match }) => {
+        var active = match ? "customli" : "";
+        return (
+          <li className={active}>
+            <NavLink to={to} className="nav-link">
+              {label}
+            </NavLink>
+          </li>
+        );
+      }}
+    />
+  );
+};
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <nav className="navbar navbar-expand-lg bg-dark">
+        <nav className="navbar navbar-expand-lg bg-light">
           <div className="container-fluid">
-            <a className="navbar-brand color-nav" href="/#">
-              Yama
-            </a>
             <div className="collapse navbar-collapse" id="navbarNav">
               <ul className="navbar-nav">
                 <li className="nav-item">
-                  <a
-                    className="nav-link color-nav"
-                    aria-current="page"
-                    href="/#"
-                  >
-                    Home
-                  </a>
+                  <NavLink to="/" className="nav-link">
+                    Yama
+                  </NavLink>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link color-nav" href="/#">
+                  <NavLink to="contact" className="nav-link">
+                    Contact
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink to="about" className="nav-link">
                     About
-                  </a>
+                  </NavLink>
                 </li>
               </ul>
             </div>
@@ -37,6 +53,7 @@ class App extends Component {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
         </Routes>
       </div>
     );
---
+++ src/components/NotFound.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+    render() {
+        return (
+            <div>
+                Oops, can not found this site
+            </div>
+        );
+    }
+}
+
+export default NotFound;
