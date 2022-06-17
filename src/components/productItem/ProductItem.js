3c706781d348a06772a23996f99b88e3b1acfc81
Yano Omurai <52166982+thanhtaimrjoe@users.noreply.github.com>
2022-06-17 11:05:26 +0700

    Update ProductItem.js

--- src/components/productItem/ProductItem.js
+++ src/components/productItem/ProductItem.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class ProductItem extends Component {
   onDelete = (id) => {
@@ -20,9 +21,12 @@ class ProductItem extends Component {
           <span className={`badge bg-${statusClass}`}>{statusName}</span>
         </td>
         <td>
-          <button type="button" className="btn btn-warning me-2">
+          <Link
+            to={`/product/${product.id}/edit`}
+            className="btn btn-warning me-2"
+          >
             Edit
-          </button>
+          </Link>
           <button
             type="button"
             className="btn btn-danger"
